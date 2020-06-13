4daa1a41911c0cd199571e12715aef2cd8c96da3
Leonardo Guarilha <lsguarilha01@gmail.com>
2020-06-13 19:54:47 -0300

    appgobarber Finish signin page

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextInputProps } from 'react-native';
 
-import { Container, TextInput } from './styles';
+import { Container, TextInput, Icon } from './styles';
 
 // Tenho todas as propriedades do input e mais o que eu adicionar
 interface InputProps extends TextInputProps {
@@ -12,6 +12,7 @@ interface InputProps extends TextInputProps {
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => (
   <Container>
     {/* Passo todo o resto das propriedades menos o name e o icon para o TextInput */}
+    <Icon name={icon} size={20} color="#666360" />
     <TextInput
       keyboardAppearance="dark"
       placeholderTextColor="#666360"
