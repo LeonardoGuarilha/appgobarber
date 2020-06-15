2cd15f4367555d92f0887f55d80f535888a590de
Leonardo Guarilha <lsguarilha01@gmail.com>
2020-06-15 17:39:21 -0300

    appgobarber create Dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -9,13 +9,20 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+  email: string;
+}
+
 interface SignInCredentials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -23,7 +30,7 @@ interface AuthContextData {
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 // Variável que vai estar disponível para a aplicação
@@ -43,6 +50,9 @@ const AuthProvider: React.FC = ({ children }) => {
 
       // A posição 0 é a chave e a posição 1 é o valor. o multGet retorna um array
       if (token[1] && user[1]) {
+        // Para o token do usuário
+        // Coloca o token no header de todas as requisições
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -65,6 +75,10 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)], // o user é um objeto. Salvo ele como string
     ]);
+
+    // Para o token do usuário
+    // Coloca o token no header de todas as requisições
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
